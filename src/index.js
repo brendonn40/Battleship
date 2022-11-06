2ac93961a400ec58d991d1f29972c6712700a2b2
brendon <brendoncesar40@gmail.com>
2022-11-06 15:54:38 -0300

    ships representation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./style.css";
 import { createBoardDisplay, createFleet, createOpponent } from "./domStuff.js";
-
+const playerFactory = require("./factories/player.js");
 const player = document.getElementById("player");
 const computer = document.getElementById("computer");
 player.appendChild(createFleet());
